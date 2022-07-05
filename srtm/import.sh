8acc7562f93bf6167e6dee005a6e4646d22fcd40
Sebastian Brodehl <brodehl@uni-mainz.de>
2022-07-05 10:57:57 +0200

    Update import command.

--- srtm/import.sh
+++ srtm/import.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-raster2pgsql -a -e -s 4326 -t 100x100 unzipped/*.hgt elevations | psql
+# raster2pgsql -a -e -s 4326 -t 100x100 unzipped/*.hgt elevations | psql
+raster2pgsql -d -M -C -I -F -s 4326 -t 25x25 unzipped/*.hgt elevation | psql -d ogn
