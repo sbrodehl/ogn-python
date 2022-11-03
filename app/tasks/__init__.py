d3caf9ed79cc38d3e666ebbbcc43dee751c35d8d
Sebastian Brodehl <foss@s14l.de>
2022-11-03 20:32:29 +0100

    Add __all__ import statement.

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -3,3 +3,13 @@ from .sql_tasks import update_statistics, update_sender_direction_statistics
 from .orm_tasks import transfer_to_database
 from .orm_tasks import update_takeoff_landings, update_logbook, update_logbook_max_altitude
 from .orm_tasks import import_ddb
+
+__all__ = [
+    "update_statistics", "update_sender_direction_statistics",
+
+    "transfer_to_database",
+
+    "update_takeoff_landings", "update_logbook", "update_logbook_max_altitude",
+
+    "import_ddb"
+]
