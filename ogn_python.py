df915b57d70c151e0a52862455289f952fc1a789
Sebastian Brodehl <foss@s14l.de>
2022-11-03 19:08:16 +0100

    PEP fixes.

--- ogn_python.py
+++ ogn_python.py
@@ -5,6 +5,7 @@ from app import create_app, db, commands
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 commands.register(app)
 
+
 @app.shell_context_processor
 def make_shell_context():
     return dict(app=app, db=db)
