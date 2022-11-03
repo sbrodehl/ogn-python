443b479e3cdc367434ef6d01e9fe6d69fe4c2cd7
Sebastian Brodehl <foss@s14l.de>
2022-11-03 20:35:57 +0100

    Ignore qa for imports.

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,8 +2,8 @@ from flask import Blueprint
 
 bp = Blueprint("main", __name__)
 
-import app.main.routes
-import app.main.jinja_filters
+import app.main.routes  # noqa
+import app.main.jinja_filters  # noqa
 
 __all__ = [
     "routes", "jinja_filters", "bp"
